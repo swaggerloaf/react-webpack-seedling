e79572d23420f400439d680bef950707dfc33fe7
swaggerloaf <swaggerloaf@gmail.com>
2021-05-08 18:59:33 -0400

    added css

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import styles from  './style.css';
+
+const App = ({ message }: any) => <div className={styles.hello}>{message}</div>;
+
+ReactDOM.render(
+  <App message={'Hi Fred'}></App>,
+  document.getElementById('root')
+)

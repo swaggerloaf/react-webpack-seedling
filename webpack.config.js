2f785a51e1b3903f7541a895431eb0c43f380d09
swaggerloaf <swaggerloaf@gmail.com>
2021-06-06 14:51:25 -0400

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,10 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   },

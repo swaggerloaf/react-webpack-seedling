a282305f537f4ba57868f67cb8d345d7fbecca5a
swaggerloaf <swaggerloaf@gmail.com>
2021-12-11 19:58:56 -0500

    react 18

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './styles.scss';
+// eslint-disable-next-line no-unused-vars
+import React from "react"
+import ReactDOM from "react-dom"
+import "./styles.scss"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<div className="text-primary">Hi there!</div>);
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<div className="text-primary">Hi there!</div>)
 
 // render(
 //   <div className="text-danger">Hi there!</div>,

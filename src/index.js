2ae898ae8a36e45605c515dd360d04269c916b4c
swaggerloaf <swaggerloaf@gmail.com>
2021-12-11 19:22:19 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './styles.scss';
 
-render(<div>Hi there!</div>, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<div className="text-primary">Hi there!</div>);
+
+// render(
+//   <div className="text-danger">Hi there!</div>,
+//   document.getElementById('root')
+// );
